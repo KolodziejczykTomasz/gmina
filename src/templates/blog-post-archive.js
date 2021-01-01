220169af2563b0f23c67e6632c1374ad1f95505b
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2021-01-01 21:16:14 +0100

    Added menu in BreakeSection.

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import parse from "html-react-parser"
 
 import Layout from "../components/layout"
@@ -23,6 +23,7 @@ const MainWrapperSection = styled.div`
 const SectionWrapper = styled.div`
   display: grid;
   grid-template-columns: 0.25fr 0.75fr;
+  height: auto;
 `
 const SectionSideLeft = styled.div`
   width: 100%;
@@ -30,7 +31,7 @@ const SectionSideLeft = styled.div`
 `
 const SectionSideRight = styled.div`
   width: 100%;
-  height: 100%;
+  height: auto;
 `
 
 const SectionNews = styled.div`
@@ -64,15 +65,66 @@ const SectionCulture = styled.div`
   height: auto;
 `
 
+const CardWrapper = styled.div`
+  height: 100%;
+`
+
+const CardHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const CardHeaderTitle = styled.div`
+  background-color: red;
+`
+const CardHeaderDate = styled.div`
+  background-color: green;
+`
+
+const CardMain = styled.div`
+  display: grid;
+  grid-template-columns: 0.3fr 0.7fr;
+`
+const CardMainPhoto = styled.div`
+  background-color: blue;
+`
+const CardMainContent = styled.div`
+  background-color: burlywood;
+`
+
+const CardFooter = styled.div`
+  background-color: grey;
+`
+const ButtonMore = styled.button``
+
+const MenuListWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  color: #fff;
+`
+
+const MenuListItem = styled.div`
+  padding: 10px 15px;
+  width: auto;
+  &:hover {
+    color: initial;
+    cursor: pointer;
+    background-color: #fff;
+  }
+`
+
 const BlogIndex = ({ data }) => {
   const posts = data.allWpPost.nodes
-
   return (
     <Layout>
       <MainWrapper>
         <SEO title="UG Gmina" />
         <MainWrapperSection>
-          <BreakeSection>Aktualności</BreakeSection>
+          <BreakeSection>
+            <MenuListWrapper>
+              <MenuListItem>Wszystkie aktualności</MenuListItem>
+            </MenuListWrapper>
+          </BreakeSection>
           <SectionNews>
             <SectionWrapper>
               <SectionSideLeft>Aside Photo</SectionSideLeft>
@@ -80,22 +132,23 @@ const BlogIndex = ({ data }) => {
                 <ol style={{ listStyle: `none`, marginLeft: "-35px" }}>
                   {posts.map(post => {
                     const title = post.title
-
                     return (
                       <li key={post.uri}>
-                        <article itemScope itemType="http://schema.org/Article">
-                          <header>
-                            <h2>
-                              <Link to={post.uri} itemProp="url">
-                                <span itemProp="headline">{parse(title)}</span>
-                              </Link>
-                            </h2>
-                            <small>{post.date}</small>
-                          </header>
-                          <section itemProp="description">
-                            {parse(post.excerpt)}
-                          </section>
-                        </article>
+                        <CardWrapper>
+                          <CardHeader>
+                            <CardHeaderTitle>{parse(title)}</CardHeaderTitle>
+                            <CardHeaderDate>{post.date}</CardHeaderDate>
+                          </CardHeader>
+                          <CardMain>
+                            <CardMainPhoto>PHOTO</CardMainPhoto>
+                            <CardMainContent>
+                              {parse(post.excerpt)}
+                            </CardMainContent>
+                          </CardMain>
+                          <CardFooter>
+                            <ButtonMore>Zobacz więcej</ButtonMore>
+                          </CardFooter>
+                        </CardWrapper>
                       </li>
                     )
                   })}
@@ -103,35 +156,64 @@ const BlogIndex = ({ data }) => {
               </SectionSideRight>
             </SectionWrapper>
           </SectionNews>
-          <BreakeSection>Gmina</BreakeSection>
+          <BreakeSection>
+            <MenuListWrapper>
+              <MenuListItem>Ogłoszenia</MenuListItem>
+              <MenuListItem>Urząd Gminy</MenuListItem>
+              <MenuListItem>Sprzedaż nieruchomości</MenuListItem>
+              <MenuListItem>Dotacje</MenuListItem>
+              <MenuListItem>RODO</MenuListItem>
+            </MenuListWrapper>
+          </BreakeSection>
           <SectionAbout>
             <SectionWrapper>
               <SectionSideLeft>Aside Photo</SectionSideLeft>
-              <SectionSideRight>Gmina</SectionSideRight>
+              <SectionSideRight></SectionSideRight>
             </SectionWrapper>
           </SectionAbout>
-          <BreakeSection>History</BreakeSection>
+          <BreakeSection>
+            <MenuListWrapper>
+              <MenuListItem>Historia Gminy</MenuListItem>
+              <MenuListItem>Zabytki</MenuListItem>
+            </MenuListWrapper>
+          </BreakeSection>
           <SectionHistory>
             <SectionWrapper>
               <SectionSideLeft>Aside Photo</SectionSideLeft>
               <SectionSideRight>Gmina</SectionSideRight>
             </SectionWrapper>
           </SectionHistory>
-          <BreakeSection>Natura</BreakeSection>
+          <BreakeSection>
+            <MenuListWrapper>
+              <MenuListItem>Gospodarka odpadami</MenuListItem>
+              <MenuListItem>Zwierzęta bezdomne</MenuListItem>
+              <MenuListItem>Ochrona środowiska</MenuListItem>
+            </MenuListWrapper>
+          </BreakeSection>
           <SectionNatura>
             <SectionWrapper>
               <SectionSideLeft>Aside Photo</SectionSideLeft>
               <SectionSideRight>Gmina</SectionSideRight>
             </SectionWrapper>
           </SectionNatura>
-          <BreakeSection>Tourism</BreakeSection>
+          <BreakeSection>
+            <MenuListWrapper>
+              <MenuListItem>Atrakcje turystyczne</MenuListItem>
+              <MenuListItem>Noclegi</MenuListItem>
+              <MenuListItem>Sport</MenuListItem>
+            </MenuListWrapper>
+          </BreakeSection>
           <SectionTourism>
             <SectionWrapper>
               <SectionSideLeft>Aside Photo</SectionSideLeft>
               <SectionSideRight>Gmina</SectionSideRight>
             </SectionWrapper>
           </SectionTourism>
-          <BreakeSection>Culture</BreakeSection>
+          <BreakeSection>
+            <MenuListWrapper>
+              <MenuListItem>Atrakcje kulturalne</MenuListItem>
+            </MenuListWrapper>
+          </BreakeSection>
           <SectionCulture>
             <SectionWrapper>
               <SectionSideLeft>Aside Photo</SectionSideLeft>
